d598025710299a6926444bfb80c7f1e7e431dbab
Ashley Sands <ashsands1@amstrike.com>
2020-01-15 20:04:16 +0000

    initial commit

---
+++ Editor/BaseListView.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+
+public class SceneComponentListView<T> where T : MonoBehaviour
+{
+
+    // store all components in a dict where the key is the scene name.
+    private Dictionary<string, List<T>> sceneCompoents = new Dictionary<string, List<T>>();
+
+    public void GetComponentsFromObject( GameObject obj )
+    {
+        AddComponents( obj.scene.name, obj.GetComponentsInChildren<T>() );
+    }
+
+    public void GetComponentsFromScene( Scene scene )
+    {
+        GameObject[] gameObjs = scene.GetRootGameObjects();
+
+        foreach ( GameObject go in gameObjs )
+            GetComponentsFromObject( go );
+
+    }
+
+    private void AddComponents( string key, T[] components)
+    {
+        // add the key if it does not exist
+        if ( !sceneCompoents.ContainsKey(key) )
+            sceneCompoents.Add( key, new List<T>() );
+
+        sceneCompoents[ key ].AddRange( components );
+
+    }
+
+    public T[] GetComponentsFromScene(string sceneName )
+    {
+        if ( !sceneCompoents.ContainsKey( sceneName ) )
+            return new T[0];
+
+        return sceneCompoents[ sceneName ].ToArray();
+    }
+
+    public string[] GetSceneNames()
+    {
+        return new List<string>( sceneCompoents.Keys ).ToArray();
+    }
+
+}
